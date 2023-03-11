7b4bed96b0c13f9e5bfd1ec70b479944f906c469
Иван Дрей <dreyvania999@gmail.com>
2023-03-11 13:05:39 +0300

    Изменения

--- UPM/MainWindow.xaml.cs
+++ UPM/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows;
 using System.Linq;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System;
 
 namespace UPM
 {
@@ -12,7 +13,7 @@ namespace UPM
     public partial class MainWindow : Window
     {
        
-
+        Staff user = null;
         public MainWindow()
         {
             DBClass.DB = new Entities1();
@@ -22,12 +23,19 @@ namespace UPM
            
             UserEditing.DisplayMemberPath = "StaffName";
             UserEditing.SelectedValuePath = "ID";
-            ListInform.ItemsSource = DBClass.DB.StaffInform.ToList();
+            
         }
        
         private void Grid_LostFocus(object sender, RoutedEventArgs e)
         {
             Icons.MaxWidth = Icons.ActualWidth / 2;
+
+        }
+
+        private void UserEditing_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            user = DBClass.DB.Staff.FirstOrDefault(x => x.ID == Convert.ToInt32( UserEditing.SelectedValue.ToString()));
+            //ListInform.ItemsSource = DBClass.DB.StaffInform.Where(x=>x.Role== ).ToList();
 
         }
     }
